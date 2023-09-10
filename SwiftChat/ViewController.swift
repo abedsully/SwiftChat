f871e66f6913cd8c3b63f9cb795998ef9007766b
abedsully <abedw2909@gmail.com>
2023-09-10 15:42:45 +0700

    Initial Commit

---
+++ SwiftChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftChat
+//
+//  Created by Stefanus Albert Wilson on 9/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
